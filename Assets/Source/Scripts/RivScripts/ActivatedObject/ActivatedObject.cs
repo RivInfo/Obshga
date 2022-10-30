622c9a588c95eae7c6a23b59c5880c6c5957729c
Роман Иванов <romaiv99@gmail.com>
2022-10-30 15:46:10 +0300

    Изменил структуру папок

---
+++ Assets/Source/Scripts/RivScripts/ActivatedObject/ActivatedObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ActivatedObject : MonoBehaviour
+{
+    public abstract void ActivateObject();
+    public abstract void DeactivateObject();
+}
